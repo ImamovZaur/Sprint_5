5fb7b556b5c804a7819db28fddea7b956c782a3a
Заур Имамов <alltrash1001@bk.ru>
2025-03-23 23:52:53 +0300

    Sprint_5

---
+++ conftest.py
@@ -0,0 +1,29 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import const
+from locators import Locators
+
+
+@pytest.fixture
+def driver():
+    browser = webdriver.Chrome()
+    browser.get(const.MAIN_URL)
+    yield browser
+    browser.quit()
+
+@pytest.fixture
+def logged_user(driver):
+    driver.get(const.LOGIN_PAGE)
+    WebDriverWait(driver, 3).until(EC.element_to_be_clickable((Locators.LOGIN_EMAIL_INPUT)))
+    driver.find_element(*Locators.LOGIN_EMAIL_INPUT).send_keys(const.EMAIL_FOR_LOGIN)
+    driver.find_element(*Locators.LOGIN_PASSWORD_INPUT).send_keys(const.PASSWORD_FOR_LOGIN)
+    driver.find_element(*Locators.LOGIN_BUTTON).click()
+    WebDriverWait(driver, 3).until(EC.element_to_be_clickable((Locators.PERSONAL_ACCOUNT)))
+    driver.find_element(*Locators.PERSONAL_ACCOUNT).click()
+    return driver
+
+
+def wait_for_element_located(driver, locator, time, condition):
+    return WebDriverWait(driver, time).until(condition(locator))
---
+++ const.py
@@ -0,0 +1,11 @@
+MAIN_URL = ('https://stellarburgers.nomoreparties.site')
+
+LOGIN_PAGE = ('https://stellarburgers.nomoreparties.site/login')
+
+PROFILE_CABINET = ('https://stellarburgers.nomoreparties.site/account/profile')
+
+EMAIL_FOR_LOGIN = ('mr.zaur94@yandex.ru')
+
+PASSWORD_FOR_LOGIN = ('qwerty777')
+
+NAME_FOR_REGISTRATION = ('Заур Практикум')
